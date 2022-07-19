fc0d4d40ec7ea2796ce604c3a8882e4386b4fdee
Sabiha Bary <sabihaab9@gmail.com>
2022-07-19 12:44:50 -0400

    commit tree 224811df2f2f967f7f2e7af1dcc7a6189f687d09

--- db/migrate/01_create_students.rb
+++ db/migrate/01_create_students.rb
@@ -1,4 +1,9 @@
 class CreateStudents < ActiveRecord::Migration[6.1]
+  
+  #define a change method
   def change
+    #use the active record create_table method
+    create_table :artists do |t|
+      t.string :name
   end
 end
